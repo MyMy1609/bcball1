adcabbe13e7c4210bf187db5204aae28fbe1e316
Thy <anhthy3103@gmail.com>
2016-10-21 14:16:33 +0700

    Resolve conflict in bg color

--- BouncingBall.java
+++ BouncingBall.java
@@ -116,7 +116,8 @@ public class BouncingBall extends JFrame {
 		protected void paintComponent(Graphics g) {
 			long start = System.nanoTime();
 			super.paintComponent(g);
-			setBackground(Color.WHITE);
+			setBackground(new Color(175,215,230));
+			//setBackground(Color.BLUE);
 			//Ball
 			g.setColor(ballColor);
 			g.fillOval(x, y, size, size);
